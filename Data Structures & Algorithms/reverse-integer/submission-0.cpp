a2dd48b739c9e3b69c40dd123321f9520b4754ef
Ishan Vohra <ivohra_be23@thapar.edu>
2026-08-18 22:28:08 +0530

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int rem = x % 10;
+            x /= 10;
+
+            // Check positive overflow
+            if (rev > INT_MAX / 10 || 
+                (rev == INT_MAX / 10 && rem > 7)) {
+                return 0;
+            }
+
+            // Check negative overflow
+            if (rev < INT_MIN / 10 || 
+                (rev == INT_MIN / 10 && rem < -8)) {
+                return 0;
+            }
+
+            rev = rev * 10 + rem;
+        }
+
+        return rev;
+    }
+};

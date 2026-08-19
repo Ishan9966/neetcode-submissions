326d2aa6e04ef3bc68883287a24a98bc83335387
Ishan Vohra <ivohra_be23@thapar.edu>
2026-08-19 16:11:53 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+
+        if(digits[n - 1] < 9) {
+            digits[n - 1]++;
+        }
+        else {
+            for(int i = n - 1; i >= 0; i--) {
+                if(digits[i] < 9) {
+                    digits[i]++;
+                    return digits;
+                }
+
+                digits[i] = 0;
+            }
+
+            digits.insert(digits.begin(), 1);
+        }
+
+        return digits;
+    }
+};

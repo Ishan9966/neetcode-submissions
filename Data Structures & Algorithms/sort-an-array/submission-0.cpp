1a969a3522ae214c6b483d5d94b0293748a94e2c
Ishan Vohra <ivohra_be23@thapar.edu>
2026-09-01 09:51:21 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        priority_queue<int,vector<int>,greater<>> pq;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+        }
+        for(int i=0;i<nums.size();i++){
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
+};

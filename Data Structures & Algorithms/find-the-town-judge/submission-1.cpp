86c3c90c9f0787f0d9018d6f3208d40c54b4f208
Ishan Vohra <ivohra_be23@thapar.edu>
2026-09-20 16:42:01 +0530

    Add: find-the-town-judge - submission-1

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> score(n + 1,0);
+
+        for (int i = 0; i < trust.size(); i++) {
+            score[trust[i][0]]--;
+            score[trust[i][1]]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (score[i] == n - 1) return i;
+        }
+        return -1;
+    }
+};

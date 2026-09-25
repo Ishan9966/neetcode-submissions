13cd8482beb10fb2e3e1bd9f64f37e01a386177c
Ishan Vohra <ivohra_be23@thapar.edu>
2026-09-25 15:18:01 +0530

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    void dfs(vector<int>& nums, vector<vector<int>>& ans,int i) {
+    if(i==nums.size()){
+        ans.push_back(nums);
+        return;
+    }
+    for(int a=i;a<nums.size();a++){
+        swap(nums[i],nums[a]);
+        dfs(nums,ans,i+1);
+        swap(nums[i],nums[a]);
+    }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+       vector<vector<int>> ans;
+        dfs(nums, ans,0);
+        return ans;
+    }
+};
